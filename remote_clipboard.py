c934e164895f342f8b3c3ee87fb3505ddc69847a
clach04 <clach04@gmail.com>
2020-04-27 00:00:27 -0700

    qrcode for clipboard (py2 only)

--- remote_clipboard.py
+++ remote_clipboard.py
@@ -5,8 +5,14 @@
 """View and change clipboard remotely
 """
 
+import urllib  # FIXME py2 only
 import os
+import socket
 import sys
+try:
+    import webbrowser
+except ImportError:
+    webbrowser = None
 from wsgiref.simple_server import make_server
 try:
     from cgi import escape
@@ -27,6 +33,45 @@ try:
 except ImportError:
     android = droid = None
     import xerox  # NOTE use https://github.com/clach04/xerox/tree/win_no_crash until PR merged -- from https://github.com/kennethreitz/xerox
+
+
+def gen_qrcode_url(url, image_size=547):
+    """Construct QR generator google URL with max size, from:
+
+    https://chart.googleapis.com/chart? - All infographic URLs start with this root URL, followed by one or more parameter/value pairs. The required and optional parameters are specific to each image; read your image documentation.
+        chs - Size of the image in pixels, in the format <width>x<height>
+        cht - Type of image: 'qr' means QR code.
+        chl - The data to encode. Must be URL-encoded.
+
+    See https://google-developers.appspot.com/chart/infographics/docs/overview
+    """
+    url = urllib.quote(url)
+    image_size_str = '%dx%d' % (image_size, image_size)
+    result = 'https://chart.googleapis.com/chart?cht=qr&chs=%s&chl=%s' % (image_size_str, url)
+    return result
+
+# Utility function to guess the IP (as a string) where the server can be
+# reached from the outside. Quite nasty problem actually.
+
+def find_ip ():
+   # we get a UDP-socket for the TEST-networks reserved by IANA.
+   # It is highly unlikely, that there is special routing used
+   # for these networks, hence the socket later should give us
+   # the ip address of the default route.
+   # We're doing multiple tests, to guard against the computer being
+   # part of a test installation.
+
+   candidates = []
+   for test_ip in ["192.0.2.0", "198.51.100.0", "203.0.113.0"]:
+      s = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
+      s.connect ((test_ip, 80))
+      ip_addr = s.getsockname ()[0]
+      s.close ()
+      if ip_addr in candidates:
+         return ip_addr
+      candidates.append (ip_addr)
+
+   return candidates[0]
 
 
 def copy(new_text):
@@ -109,6 +154,14 @@ def doit():
     port = 8000
     print('Open http://%s:%d' % (hostname, port))
     print('Issue CTRL-C (Windows CTRL-Break instead) to stop')
+
+    ip_addr = find_ip()
+    url_str = "http://%s:%s/" % (ip_addr, port)
+    print(url_str)
+    if webbrowser:
+        qrcode_url = gen_qrcode_url(url_str)
+        webbrowser.open(qrcode_url)
+
     httpd = make_server(hostname, port, application)
     httpd.serve_forever()
 
